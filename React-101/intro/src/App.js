0c385af77f46ebf1e09020069d538eef29fd3e0d
Danyal Can Kayrak <dancankan@gmail.com>
2023-04-03 23:07:22 +0300

    Finished Component Fundamentals

---
+++ React-101/intro/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import Navi from './Navi'
+import ProductList from './ProductList';
+import Category from './CategoryList';
+import CategoryList from './CategoryList';
+import { Container, Row, Col } from 'reactstrap';
+
+
+export default class App extends Component {
+
+  state={currentCategory:""}
+  
+  changeCategory = (category)=>{
+    this.setState({currentCategory:category.categoryName})
+  }
+
+  render() {
+    let categoryInfo = { title: "Category List" };
+    let productInfo = { title: "Product List" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+
+
+      </div>
+    );
+  }
+}
---
+++ React-101/intro/src/CategoryList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
+
+export default class CategoryList extends Component {
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Yemek" },
+      { categoryId: 2, categoryName: "Içecek" }
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <h3>{this.props.info.title}</h3>
+        <ListGroup>
+          {
+            this.state.categories.map(category => (
+              <ListGroupItem onClick={()=>this.props.changeCategory(category)} key={category.categoryId}>
+                {category.categoryName}
+              </ListGroupItem>
+            ))
+          }
+        </ListGroup>
+        <h4>{this.props.currentCategory}</h4>
+      </div>
+    )
+  }
+}
